e1e94420cdf2f3e7e59b0e17a16770303d01a516
Joseph Olaide <73533435+Josepholaidepetro@users.noreply.github.com>
2021-03-21 00:24:41 +0100

    Update tfjob.py

--- tfjob.py
+++ tfjob.py
@@ -93,7 +93,7 @@ def main(args):
   # number of steps per epoch. Note that the numbers here are for demonstration
   # purposes only and may not sufficiently produce a model with good quality.
   multi_worker_model.fit(train_datasets_sharded,
-                         epochs=7,
+                         epochs=4,
                          steps_per_epoch=70,
                          callbacks=callbacks)
   
